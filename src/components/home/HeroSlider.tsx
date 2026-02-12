1d580b35ab0fb44c6380e1e5ab86767b25202f61
razvancretoiu-wq <razvancretoiu@gmail.com>
2026-02-12 07:46:00 +0200

    Fix hero text alignment on mobile

--- src/components/home/HeroSlider.tsx
+++ src/components/home/HeroSlider.tsx
@@ -15,7 +15,7 @@ export default function HeroSlider() {
         priority
       />
 
-      <div className="relative z-10 max-w-7xl mx-auto px-6 text-left md:text-center">
+      <div className="relative z-10 max-w-7xl mx-auto px-6 text-left md:text-left">
         <motion.h1
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
