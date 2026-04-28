dce21d6a7a0ddafe7676f808a4e79fb7975bf3a7
razvancretoiu-wq <razvancretoiu@gmail.com>
2026-04-28 21:38:06 +0300

    Replace promo grid with product carousels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import HeroSlider from "@/components/home/HeroSlider"
 import BenefitsBar from "@/components/home/BenefitsBar"
 import ProductCategoryCarousels from "@/components/ProductCategoryCarousels"
-import ProductTabs from "@/components/home/ProductTabs"
-
 
 export default function Home() {
   return (
     <>
       <HeroSlider />
       <BenefitsBar />
-      <PromoGrid />
-      <ProductTabs />
+      <ProductCategoryCarousels />
     </>
   )
 }
